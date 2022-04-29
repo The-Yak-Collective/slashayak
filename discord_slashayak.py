80e27385623b8bc86d9fa6e2088acffb16494a6f
tilnow <44685516+tilnow@users.noreply.github.com>
2022-04-29 14:23:18 +0300

    first try of slashayak

---
+++ discord_slashayak.py
@@ -0,0 +1,16 @@
+#separate files so we can split up robot between multiple files, if we want
+
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+intents = discord.Intents.default()
+intents.members = True
+intents.messages=True
+intents.message_content=True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+#maybe not bot but slash?
+#bot = commands.Bot(command_prefix='$$', intents=intents)
+#print('bot:',bot)
---
+++ slashayak.py
@@ -0,0 +1,40 @@
+## parts from here: https://gist.github.com/Rapptz/c4324f17a80c94776832430007ad40e6#slash-commands-and-context-menu-commands
+
+    
+
+
+
+from discord.ext import tasks, commands
+import discord
+import asyncio
+import os
+import re
+import subprocess
+import time
+import datetime
+from dotenv import load_dotenv
+
+import logging
+
+
+from discord_slashayak import * #including client and tree
+
+HOME_DIR="/home/yak/robot/slashayak/"
+USER_DIR="/home/yak/"
+
+load_dotenv(USER_DIR+'.env')
+
+@tree.command()
+@app_commands.describe(echome='text to echo')
+async def slashatest(interaction: discord.Interaction, echome: str):
+    await interaction.response.send_message(f'{echome=}', ephemeral=True)
+
+@client.event #needed since it takes time to connect to discord
+async def on_ready(): 
+    print("slashayak is up!")
+
+    return
+
+
+discord_token=os.getenv('SLASHAYAK_DISCORD_KEY')
+client.run(discord_token) 
