60704e7ccf911e316fb7e3af24657e90d4796b5b
tilnow <44685516+tilnow@users.noreply.github.com>
2022-04-29 15:16:16 +0300

    trying to port unfurl
    later to add thread unfurl

--- slashayak.py
+++ slashayak.py
@@ -29,12 +29,61 @@ load_dotenv(USER_DIR+'.env')
 async def slashatest(interaction: discord.Interaction, echome: str):
     await interaction.response.send_message(f'{echome=}', ephemeral=True)
 
+@tree.command()
+@app_commands.describe(linktounfurl='the link, also from a thread, to unfurl. single link only, sorry. latter to add a whole thread, i guess')
+async def tfurl(interaction: discord.Interaction, thelink: str):
+
+    message=interaction.message #there are actually cooler tools around interactions. for a later time...
+    
+    url=[None,linktounfurl] #just for backwards compatibility and prevent need to debug, for now. clean later
+
+    try:
+        #print(temp_l,url[1],':',durl2m(url[1]))
+        m,chan,c=await durl2m(url[1])
+        #print("afterdurl")
+        txt=m.content
+        strig="<@"+str(m.author.id)+"> in <#"+chan+">:\n"+txt
+        #print(strig)
+        #await message.channel.send(strig) needs split as message could be long (2k chars. maybe better solution is simply to send two messages? tried it and it does not work. strange)
+        await splitsend(message.channel,strig,False)
+    except:
+        await message.channel.send("some bug. are you sure that is a link to a discord message?")
+
+        return
+
+    await interaction.response.send_message("hope you got the output you wanted",ephemeral=True)
+
 @client.event #needed since it takes time to connect to discord
 async def on_ready(): 
-    print("slashayak is up!")
+
     await tree.sync()
+    print("slashayak is up!")
     return
 
+async def durl2m(u): #needs to be redone for thread...
+    print(u)
+    url=u.split("/")
+    url=list(reversed(url))
+    print(url)
+    c=client.guilds[0].get_channel(int(url[1]))
+    m=await c.fetch_message(int(url[0]))
+    return m,url[1],c
+
+async def splitsend(ch,st,codeformat):
+#send data in chunks smaller than 2k
+#might it have a bug of dropping last space and last line?
+    if len(st)<1900: #discord limit is 2k and we want some play)
+        if codeformat:
+            await ch.send('```'+st+'```')
+        else:
+            await ch.send(st)
+    else:
+        x=st.rfind('\n',0,1900)
+        if codeformat:
+            await ch.send('```'+st[0:x]+'```')
+        else:
+            await ch.send(st[0:x])
+        await splitsend(ch,st[x+1:],codeformat)
 
 discord_token=os.getenv('SLASHAYAK_DISCORD_KEY')
 client.run(discord_token) 
