4eb4f62989e1c2ae3a69a20653e055fb2942fed4
tilnow <44685516+tilnow@users.noreply.github.com>
2022-06-28 23:22:33 +0300

    add auto pulsing

--- slashayak.py
+++ slashayak.py
@@ -3,7 +3,7 @@
     
 
 
-
+from discord.app_commands import Choice
 from discord.ext import tasks, commands
 import discord
 import asyncio
@@ -30,6 +30,12 @@ conn=sqlite3.connect(HOME_DIR+'slashayakdatabase.db') #the connection should be 
 
 db_c = conn.cursor()
 
+@tasks.loop(minutes=60*11)
+async def pulseall():
+    pulseus=db_c.execute('select threadid from pulses').fetchall()
+    print('will now pulse ',pulseus)
+    for i in pulseus:
+        pulse(chan(i[0]))
 
 @tree.command(description= "unfurl single messages in and from threads as well as regular channels") # at this time only single link only, sorry. later to add a whole thread, i guess
 @app_commands.describe(linktounfurl='the link to unfurl')
@@ -98,21 +104,42 @@ async def promptshow(interaction: discord.Interaction):
 async def slashatest(interaction: discord.Interaction, echome: str):
     await interaction.response.send_message(f'{echome=}', ephemeral=True)
 
-@tree.command(description="pulse a thread - will it come back to life?")
+@tree.command(description="pulse a thread - will it come back to life? (yes!)")
 @app_commands.describe(threadid='ID of thread we want to pulse')
 async def slashapulse(interaction: discord.Interaction, threadid: str):
-    try:
-        th=client.guilds[0].get_channel_or_thread(int(threadid))
-        await th.send('staying alive1', delete_after=10)
-    except:
-        try:
-            th=client.guilds[0].get_thread(int(threadid))
-            await th.send('staying alive2', delete_after=10)
-        except:
+    await th=chan(i)
+    await pulse(th)
+    return
+
+async def chan(i):
+    th=client.guilds[0].get_channel_or_thread(int(threadid))
+    if th==None:
+        th=client.guilds[0].get_thread(int(threadid))
+        if th==None:
             th=await client.guilds[0].fetch_channel(int(threadid))
-            await th.send('staying alive3', delete_after=10)
+    return th
+    
+@tree.command(description="pulse a thread every 11 hours")
+@app_commands.describe(onoff='to pulse current thread or not')
+@app_commands.choices(onoff=[
+    Choice(name='on', value=1),
+    Choice(name='off', value=0),])
+
+async def pulseaday(interaction: discord.Interaction, onoff: Choice[int]): #actually every 11 hours
+    thid=interaction.channel_id
+    print ('pulse a day',thid,reaction.value)
+    if(reaction.value==1):
+        await pulse(interaction.channel)
+        db_c.execute('''insert into pulses values (NULL,?)''',(thid,))
+    if(reaction.value==0):
+        db_c.execute('''delete from pulses where threadid=? ''',(thid,))
+    conn.commit()
 
     return
+
+async def pulse(th):
+    await th.send('staying alive', delete_after=10)
+
 
 @client.event #needed since it takes time to connect to discord
 async def on_ready(): 
@@ -124,6 +151,7 @@ async def on_ready():
     checkon_database()
     print("slashayak is up!")
     print([x.name for x in tree.get_commands()])
+    pulseall.start()
     return
 
 async def durl2m(u): #needs to be redone for thread...
@@ -143,6 +171,10 @@ def checkon_database():
         db_c.execute('''CREATE TABLE prompts (promptid INTEGER PRIMARY KEY, creatorid text, contents text, filled int, createdat int, filledat int, chan int, mlink text)''') 
         #filled=is it active
         #most items will not be used...
+        conn.commit()
+    db_c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='pulses' ''')
+    if db_c.fetchone()[0]!=1:
+        db_c.execute('''CREATE TABLE pulses (entry INTEGER PRIMARY KEY, threadid INTEGER)''') 
         conn.commit()
 
 
